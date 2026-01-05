24f7bb872116c5c87602da5bad894147d8c82edb
FelipeMulato <felipe.a.mulato@gmail.com>
2026-01-05 15:30:51 -0300

    adding Policy Improvement

--- 00-mdp/policy.py
+++ 00-mdp/policy.py
@@ -17,3 +17,17 @@ def policy_evaluation(pi,P, gamma=1.0, theta =1e-10):
 
         prev_V = V.copy()
     return V
+#Given a MDP we use this function to improve a given policy
+def policy_improvement(V,P, gamma=1.0):
+    Q = np.zeros((len(P), len(P[0])),dytpe = np.float64)
+
+    for s in range(len(p)):
+        for a in range(len(P[s])):
+            for prob, next_state, reward, done in P[s][a]:
+                Q[s][a] += prob*(reward +gamma*\
+                                 V[next_state]*(not done))
+    
+    new_pi = lambda s: {s: a for s,a in enumerate(
+                                    np.argmax(Q,axis=1))}[s]
+    
+    return new_pi 
