0ac2040328f1b5b3a3e27a1b4bd00dce4d0a8502
FelipeMulato <felipe.a.mulato@gmail.com>
2026-01-07 17:32:57 -0300

    Fixing Bugs

--- 00-mdp/policy.py
+++ 00-mdp/policy.py
@@ -1,3 +1,4 @@
+import numpy as np
 #Given a MDP we use this function to estimate how good a policy will be
 def policy_evaluation(pi,P, gamma=1.0, theta =1e-10):
     prev_V = np.zeros(len(P))
@@ -6,8 +7,7 @@ def policy_evaluation(pi,P, gamma=1.0, theta =1e-10):
         V = np.zeros((len(P)))
 
         for s in range(len(P)):
-
-            for prob, next_state, reward, done in P[s][pi(s)]:
+            for next_state, prob,reward, done in P[s][pi[s]]:
                 V[s] += prob*(reward+gamma*\
                               prev_V[next_state]*(not done))
         
@@ -19,51 +19,54 @@ def policy_evaluation(pi,P, gamma=1.0, theta =1e-10):
     return V
 #Given a MDP we use this function to improve a given policy
 def policy_improvement(V,P, gamma=1.0):
-    Q = np.zeros((len(P), len(P[0])),dytpe = np.float64)
+    Q = np.zeros((len(P), len(P[0])),dtype = np.float64)
 
-    for s in range(len(p)):
+    for s in range(len(P)):
         for a in range(len(P[s])):
-            for prob, next_state, reward, done in P[s][a]:
+            for next_state,prob, reward, done in P[s][a]:
                 Q[s][a] += prob*(reward +gamma*\
                                  V[next_state]*(not done))
     
-    new_pi = lambda s: {s: a for s,a in enumerate(
-                                    np.argmax(Q,axis=1))}[s]
+    new_pi = np.argmax(Q, axis=1)
     
     return new_pi 
 
 def policy_iteration(P,gamma=1.0,theta=1e-10):
-    random_actions = np.random.choice(
-                                tuple(P[0].keys()),len(P))
-    pi = lambda s:{s:a for s, in enumerate(
-                             random_actions)}[s]
-    while True:
-        old_pi = {s:pi(s) for s in range(len(P))}
+    n_states = len(P)
+    n_actions = len(P[0])
 
-        V = policy_evaluation(pi,p,gamma,theta)
+    pi = np.random.randint(0, n_actions, size=n_states)
+    while True:
+        old_pi = pi.copy()
+
+        V = policy_evaluation(pi,P,gamma,theta)
 
         pi = policy_improvement(V,P,gamma)
 
-        if old_pi == {s:pi(s) for s in range(len(P))}:
+        if np.array_equal(old_pi, pi):
             break
     return V,pi
 
 
-def value_iteration(P,gamma=1.0, theta=1e-10):
-    V = np.zeros(len(P),dytpe=np.float64)
+def value_iteration(P, gamma=1.0, theta=1e-10):
+    n_states = len(P)
+    n_actions = len(P[0])
+
+    V = np.zeros(n_states)
 
     while True:
-        Q = np.zeros((len(p),len(p[0])),dytpe=np.float64)
-        for s in range(len(p)):
-            for a in range(len(P[s])):
+        Q = np.zeros((n_states, n_actions), dtype=np.float64)
+        for s in range(n_states):
+            for a in range(n_actions):
                 for prob, next_state, reward, done in P[s][a]:
-                    Q[s][a] += prob*(reward +gamma*\
-                                 V[next_state]*(not done))
-        if np.max(np.abs(V-np.max(Q,axis=1))) < theta:
+                    Q[s, a] += prob * (reward + gamma * V[next_state] * (not done))
+
+        new_V = np.max(Q, axis=1)
+
+        if np.max(np.abs(V - new_V)) < theta:
             break
-        V = np.max(Q,axis=1)
-    
-    pi = lambda s: {s: a for s,a in enumerate(
-                                    np.argmax(Q,axis=1))}[s]
-    
-    return V,pi
+
+        V = new_V.copy()
+
+    pi = np.argmax(Q, axis=1)
+    return V, pi
