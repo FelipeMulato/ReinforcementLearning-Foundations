cd9ac0b6fc99030fc0b01119b04461bbc07ac93b
FelipeMulato <felipe.a.mulato@gmail.com>
2026-01-15 12:12:34 -0300

    Adding n_step_td function

--- tabular_rl/td.py
+++ tabular_rl/td.py
@@ -42,3 +42,62 @@ def td_prediction(pi,env,
     
     return V, V_track
 
+
+def n_step_td_prediction(pi,env,
+        gamma=1.0,
+        init_alpha=0.5,
+        min_alpha=0.01,
+        alpha_decay_ratio=0.3,
+        n_steps=3,
+        n_episodes=500,
+        max_steps=100):
+    
+    n_states = env.observation_space.n
+    
+    V = np.zeros(n_states)
+    V_track = np.zeros((n_episodes,n_states))
+
+    alphas = decay_schedule(init_alpha,min_alpha, alpha_decay_ratio)
+
+    
+
+    for e in range(n_episodes):
+        state, info = env.reset()
+        states = [state]
+        rewards = []
+        T = np.inf
+        t = 0
+        while True:
+            if t < T:
+                action = pi[state]
+                next_state, reward, terminated, truncated, info = env.step(action)
+
+                rewards.append(reward)
+                states.append(state)
+
+                if terminated or truncated:
+                    T = t+1
+                else:
+                    state = next_state
+            tau = t -n_steps+1
+
+            if tau>=0:
+                G =0.0
+                for i in range(tau,min(tau+n_steps,T)):
+                    G += (gamma**(i-tau))*rewards[i]
+                if tau +n_steps <T:
+                    G += (gamma ** n_steps) * V[states[tau + n_steps]]
+
+                s_tau = states[tau]
+                V[s_tau] += alphas[e]*(G-V[s_tau])
+            if tau == T-1:
+                break
+            t+=1 
+
+            if t>=max_steps:
+                break 
+        V_track[e] = V
+    return V,V_track
+
+
+
