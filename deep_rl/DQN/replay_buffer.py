ef89763ede9a77260eced7a21524d911e747b68d
FelipeMulato <felipe.a.mulato@gmail.com>
2026-01-24 16:38:04 -0300

    Adding Replay Memory class

---
+++ deep_rl/DQN/replay_buffer.py
@@ -0,0 +1,17 @@
+from collections import deque
+import numpy as np
+
+class ReplayMemory():
+    def __init__(self,maxsize):
+        self.memory = deque([],maxsize)
+
+    def append(self,transition):
+        self.memory.append(transition)
+    
+    def sample(self,sample_size):
+        return np.random.sample(self.memory, sample_size)
+    
+    def __len__(self):
+        return len(self.memory)
+    
+        
