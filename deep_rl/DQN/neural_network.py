b56eafa2a5138bbbd29da00efd944a89b5f28029
FelipeMulato <felipe.a.mulato@gmail.com>
2026-01-24 16:37:26 -0300

    Adding DQN net class

---
+++ deep_rl/DQN/neural_network.py
@@ -0,0 +1,13 @@
+from torch import nn
+
+class DQN(nn.Module):
+    def __init__(self,ninput,h1_nodes,out_actions):
+        super().__init__()
+
+        self.fc1 = nn.Linear(ninput,h1_nodes)
+        self.out = nn.Linear(h1_nodes,out_actions)
+    
+    def foward(self,x):
+        x = nn.functional.relu(self.fc1(x))
+        x = self.out(x)
+        return x
