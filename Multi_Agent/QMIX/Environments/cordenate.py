5c3097d7759531a9e58ccf624010cae0ee114881
FelipeMulato <felipe.a.mulato@gmail.com>
2026-02-02 15:39:10 -0300

    Adding fucntion to define global state and agente observation

--- Multi_Agent/QMIX/Environments/cordenate.py
+++ Multi_Agent/QMIX/Environments/cordenate.py
@@ -1,9 +1,15 @@
+import torch
 class CordenateEnv:
     def reset(self):
         state = 0
         return state
     def step(self,action1, action2):
-        reward = 10 if action1==1 and action2==2 else 0
+        reward = 10 if action1==0 and action2==0 else 0
         state = 0
         done  = True
         return state, reward, done
+    def to_obs(self,state):
+        return torch.tensor([1.0], dtype=torch.float32)
+
+    def to_state_vec(self,state):
+        return torch.tensor([1.0], dtype=torch.float32)
