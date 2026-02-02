31f26013c55486f122c27273931720cf5b126ad8
FelipeMulato <felipe.a.mulato@gmail.com>
2026-02-02 14:45:33 -0300

    Adding decaying fucntion

--- Multi_Agent/QMIX/utils.py
+++ Multi_Agent/QMIX/utils.py
@@ -1,7 +1,23 @@
 import torch
 import random
-
+import numpy as np
 def epsilon_greedy(q_values,epsilon):
     if random.random()<epsilon:
         return random.randrange(q_values[0].shape)
     return int(torch.argmax(q_values).item())
+
+def decay_schedule(init_value,min_value,
+                    decay_ratio, max_steps,
+                    log_start = -2, log_base =10):
+    decay_steps = int(max_steps*decay_ratio)
+    rem_steps = max_steps-decay_steps
+
+    values = np.logspace(log_start, 0, decay_steps,
+                         base = log_base, endpoint =True)
+    values = (values-values.min())
+
+    values = (init_value-min_value)*values +min_value
+
+    values = np.pad(values,(0, rem_steps), 'edge')
+
+    return values
