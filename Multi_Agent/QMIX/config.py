ddbd63a9e59cc07ed7e7d812e82e1914f2f4ca93
FelipeMulato <felipe.a.mulato@gmail.com>
2026-02-07 17:57:15 -0300

    Setting new trainign parms

--- Multi_Agent/QMIX/config.py
+++ Multi_Agent/QMIX/config.py
@@ -1,4 +1,8 @@
 n_episodes=200
+n_agents = 3
+n_actions = 5
+obs_dim = 18
+state_dim = 54
 gamma=0.99       
 lr=1e-3
 epsilon_start=1.0
@@ -8,3 +12,9 @@ hidden_dim=16
 mixing_hidden_dim=16
 print_every=500
 max_steps=25
+
+batch_size = 32
+buffer_size = 50000
+target_update_interval = 200
+
+device = "cpu"
