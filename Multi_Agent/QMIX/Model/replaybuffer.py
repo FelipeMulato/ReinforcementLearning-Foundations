5f2ae13d1242df9b396c82f7a6776d365396b7cf
FelipeMulato <felipe.a.mulato@gmail.com>
2026-02-07 17:55:21 -0300

    Change to manual stack

--- Multi_Agent/QMIX/Model/replaybuffer.py
+++ Multi_Agent/QMIX/Model/replaybuffer.py
@@ -10,7 +10,17 @@ class ReplayBuffer:
     
     def sample(self, batch_size):
         batch = random.sample(self.buffer, batch_size)
-        return map(lambda x: torch.stack(x), zip(*batch))
-    
+
+        obs, state, actions, reward, next_obs, next_state, done = zip(*batch)
+        obs = torch.stack(obs)        
+        state = torch.stack(state)     
+        actions = torch.stack(actions)    
+        reward = torch.stack(reward)    
+        next_obs  = torch.stack(next_obs)   
+        next_state = torch.stack(next_state) 
+        done = torch.stack(done)       
+
+        return obs, state, actions, reward, next_obs, next_state, done
+        
     def __len__(self):
         return len(self.buffer)
